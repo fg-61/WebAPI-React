62f38f82a98395040fe11853fb437749c8275fc5
Fatih Gören <fatihgoren61@gmail.com>
2022-05-24 17:54:53 +0300

    DataAccess

---
+++ CategoryProduct/CategoryProduct.DataAccess/MyContext.cs
@@ -0,0 +1,16 @@
+﻿using CategoryProduct.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CategoryProduct.DataAccess
+{
+    public class MyContext : DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
